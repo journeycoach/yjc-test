157b6641a9c48f44e3ce9aae7baa370456288f94
john paine <johnpaine@johns-MacBook-Pro.local>
2026-03-11 14:20:57 -0500

    Replace broken Sanity CDN loader with native Javascript Fetch API

--- js/sanity-client.js
+++ js/sanity-client.js
@@ -1,7 +1,16 @@
-// Global Sanity Client Initialization
-const sanityClient = window.SanityClient.createClient({
-  projectId: '9ksnhows',
-  dataset: 'production',
-  useCdn: true, // Use the edge CDN for fast cached responses
-  apiVersion: '2024-03-10', // Use today's date for stable API versioning
-});
+const SANITY_PROJECT_ID = '9ksnhows';
+const SANITY_DATASET = 'production';
+const SANITY_API_VERSION = 'v2024-03-10';
+
+// A lightweight custom client using the native Fetch API to avoid heavy UMD bundle errors
+const sanityClient = {
+    fetch: function(query) {
+        const url = `https://${SANITY_PROJECT_ID}.api.sanity.io/${SANITY_API_VERSION}/data/query/${SANITY_DATASET}?query=${encodeURIComponent(query)}`;
+        return fetch(url)
+            .then(res => {
+                if (!res.ok) throw new Error(`Sanity API error: ${res.status}`);
+                return res.json();
+            })
+            .then(data => data.result);
+    }
+};
