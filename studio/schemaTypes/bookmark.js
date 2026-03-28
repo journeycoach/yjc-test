7cea35ea9fda28a2f5803fc780edf06467fea293
john paine <johnpaine@johns-MacBook-Pro.local>
2026-03-28 08:29:55 -0500

    Add Bookmark schema and rebuild admin

---
+++ studio/schemaTypes/bookmark.js
@@ -0,0 +1,29 @@
+export default {
+  name: 'bookmark',
+  title: 'Bookmark',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+      description: 'The name of the link (e.g., "Calendly").'
+    },
+    {
+      name: 'url',
+      title: 'URL',
+      type: 'url',
+      validation: (Rule) => Rule.required().uri({
+        scheme: ['http', 'https']
+      }),
+      description: 'The web address.'
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+      description: 'Optional details about this link.'
+    }
+  ]
+}
--- studio/schemaTypes/index.js
+++ studio/schemaTypes/index.js
@@ -2,5 +2,6 @@ import navigation from './navigation'
 import testimonial from './testimonial'
 import post from './post'
 import tool from './tool'
+import bookmark from './bookmark'
 
-export const schemaTypes = [navigation, testimonial, post, tool]
+export const schemaTypes = [navigation, testimonial, post, tool, bookmark]
